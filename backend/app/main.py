c53a51b9303e3188d117f134e24e5b2b16008edd
Vladimir Chegodaev <chegodaev.vl@yandex.ru>
2023-05-24 12:34:14 +0300

    Setted up image proxing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.core.settings import settings
 from app.routes import router as api_router
@@ -13,6 +14,11 @@ def create_app() -> FastAPI:
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     app.include_router(
         api_router, prefix=settings.API_PREFIX, dependencies=[Depends(is_authenticate)]
+    )
+    app.mount(
+        f"/{settings.MEDIA_PATH}",
+        StaticFiles(directory=settings.MEDIA_PATH),
+        name="images"
     )
     return app
 
